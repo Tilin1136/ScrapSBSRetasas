4016dc4e5e2486d4e88b050176c5231c7fd9afbd
Tilin1136 <saul.anyaipoma11@gmail.com>
2025-05-16 22:42:52 -0500

    Funciona

--- .ipynb_checkpoints/sbs_scraping-checkpoint.py
+++ .ipynb_checkpoints/sbs_scraping-checkpoint.py
@@ -16,7 +16,6 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920x1080")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36")
 chrome_options.add_argument("--remote-debugging-port=9222")
 
 # Inicializar driver
